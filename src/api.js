95096f88cc201c526e7418dfedbc010896b445ab
rambak02 <rambak02@mail.ru>
2024-05-13 13:29:03 +0700

    добавлен get запроc Leaderboard

---
+++ src/api.js
@@ -0,0 +1,13 @@
+const baseUrl = "https://wedev-api.sky.pro/api/leaderboard";
+
+export async function getLeaderboard() {
+  const response = await fetch(baseUrl, {
+    method: "GET",
+  });
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.error);
+  } else if (response.status === 200) {
+    return response.json();
+  }
+}
